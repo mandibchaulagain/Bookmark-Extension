3162dbe58b0139802845fb0c41a79736813bd518
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-18 21:17:09 +0545

    now user can input and click a button to save the link to the local storage

--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
+const Name = document.getElementById('linkName');
+
+
+
 function toPrintLink(){
     a = window.location.href;
-    console.log(a);
+    const Name1 = Name.value;//this gives the value property of the DOM element Name
+    b = localStorage.setItem(Name1,a);//stores in localStorage
+    console.log(localStorage.getItem(Name1));
 }
 
 PrintLinkButton.addEventListener('click',toPrintLink);
